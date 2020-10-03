752d249f7e8e324bc759e58f0d414b8383c01a56
Daiana Ruiz <daianaruiz13.dr@gmail.com>
2020-10-02 21:01:33 -0300

    ..

--- Trabajo Practico numero 2/empleados.c
+++ Trabajo Practico numero 2/empleados.c
@@ -1,3 +1,280 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define TRUE 1
+#define FALSE 0
+#define TAM 4
+#include "empleados.h"
 
+int initEmployees(eEmpleado listaEmpleados[],int tamanioArray)
+{
+    int retorno;
+	 retorno=-1;
+    for(int i=0;i <tamanioArray; i++)
+    {
+        listaEmpleados[i].isEmpty =TRUE;
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+
+int PedirEntero(char texto[],char textoError[])
+{
+    char auxiliar[10];
+    int numeroIngresado;
+
+    printf("%s", texto);
+    fflush(stdin);
+    scanf("%[^\n]",auxiliar);
+    while(ValidarNumero(auxiliar)==0)
+    {
+        printf("%s", textoError);
+        fflush(stdin);
+        scanf("%[^\n]",auxiliar);
+    }
+     numeroIngresado=atoi(auxiliar);
+
+    return numeroIngresado;
+}
+
+
+float PedirFlotante(char texto[],char textoError[])
+{
+    char auxiliar[10];
+    float numeroIngresado;
+
+    printf("%s", texto);
+    fflush(stdin);
+    scanf("%[^\n]",auxiliar);
+    while(ValidarFlotante(auxiliar)==0)
+    {
+        printf("%s", textoError);
+        fflush(stdin);
+        scanf("%[^\n]",auxiliar);
+    }
+     numeroIngresado=atof(auxiliar);
+
+    return numeroIngresado;
+}
+
+int ValidarFlotante(char numero[])
+{
+   int valido=1;
+   for(int i=0;i<strlen(numero);i++)
+   {
+	  if( (numero[i] < '0' || numero[i] > '9') && numero[i] !='.')
+	  {
+		  valido=0;
+	  }
+   }
+   return valido;
+}
+
+void PedirString(char texto[],char textoError[],int max, char input[])
+{
+    char auxiliar[500];
+
+    printf("%s", texto);
+    fflush(stdin);
+    scanf("%[^\n]",auxiliar);
+    while(ValidarLetras(auxiliar)==0|| strlen(auxiliar)>max-1)
+    {
+        printf("%s", textoError);
+        fflush(stdin);
+        scanf("%[^\n]",auxiliar);
+    }
+     strcpy(input,auxiliar);
+}
+
+
+ eEmpleado CargarEmpleado(eEmpleado listaEmpleados[],int tamanioArray, int indiceLibre)
+ {
+    eEmpleado unEmpleado;
+
+    PedirString("\nIngrese nombre: ","Error. Reingrese nombre: ",51,unEmpleado.nombre);
+    PedirString("Ingrese apellido: ","Error. Reingrese apellido: ",51,unEmpleado.apellido);
+    unEmpleado.sector = PedirEntero("Ingrese sector: ","Error, reingrese sector valido: ");
+    unEmpleado.sueldo= PedirFlotante("Ingrese sueldo: ","Error, reingrese sueldo valido:");
+    unEmpleado.isEmpty=FALSE;
+    unEmpleado.id=indiceLibre;
+
+    listaEmpleados[indiceLibre]=unEmpleado;
+
+    return unEmpleado;
+ }
+
+ int BuscarLibre(eEmpleado listaEmpleados[],int tamanioArray)
+ {
+     int index=-1;
+     for(int i=0;i<tamanioArray;i++)
+     {
+         if(listaEmpleados[i].isEmpty==TRUE)
+         {
+             index=i;
+             break;
+         }
+     }
+     return index;
+ }
+
+
+void MostrarNominaDeEmpleados(eEmpleado listaEmpleados[],int cant)
+{
+    printf("\n*********************************************************************************");
+    printf("\n\t \tListado de empleados: \n\n");
+    printf("ID \tNOMBRE\t\tAPELLIDO\t SECTOR\t \tSUELDO\n");
+    for(int i=0;i<cant;i++)
+    {
+        if(listaEmpleados[i].isEmpty==FALSE)
+        {
+            listaEmpleados[i].id=i;
+            printf("%2d %10s %15s %15d %20f \n", listaEmpleados[i].id,
+                                                listaEmpleados[i].nombre,
+                                                listaEmpleados[i].apellido,
+                                                listaEmpleados[i].sector,
+                                                listaEmpleados[i].sueldo );
+        }
+    }
+    printf("\n*********************************************************************************");
+}
+
+
+
+int findEmployeeById(int idIngresado, int tam, eEmpleado listaEmpleados[])
+{
+    int idValido=-1;
+    for(int i=0;i<tam;i++)
+    {
+       if(listaEmpleados[i].id==idIngresado)
+       {
+            idValido=idIngresado;
+       }
+    }
+    return idValido;
+}
+
+void ModificarUnEmpleado(eEmpleado listaEmpleados[],int idAModificar,int opcionModificar,int cant)
+{
+    char nombreModificado[51];
+    char apellidoModificado[51];
+
+
+    switch(opcionModificar)
+    {
+        case 1:
+            PedirString("Ingrese el nuevo nombre:","Error. Reingrese nombre: ",51,nombreModificado);
+            strcpy(listaEmpleados[idAModificar].nombre,nombreModificado);
+            break;
+        case 2:
+            PedirString("Ingrese el nuevo apellido:","Error. Reingrese apellido: ",51,apellidoModificado);
+            strcpy(listaEmpleados[idAModificar].apellido,apellidoModificado);
+             break;
+        case 3:
+             listaEmpleados[idAModificar].sector=PedirEntero("Ingrese nuevo sector: ","Error, reingrese sector valido: ");
+            break;
+        case 4:
+            listaEmpleados[idAModificar].sueldo=PedirEntero("Ingrese nuevo sueldo: ","Error, reingrese sueldo valido: ");
+               break;
+        default:
+                printf("\nOpcion no valida. Intente nuevamente\n");
+    }
+}
+
+
+
+int removeEmployee(eEmpleado listaEmpleados[],int contadorEmpleadosCargados, int idEliminar)
+{
+    int retorno=-1;
+    if(findEmployeeById(idEliminar,contadorEmpleadosCargados,listaEmpleados)!=-1)
+    {
+        retorno=0;
+        listaEmpleados[idEliminar].isEmpty=TRUE;
+    }
+    return retorno;
+}
+
+int ValidarLetras(char string[])
+{
+   int valido;
+   valido=1;
+
+  for(int i=0;i<strlen(string);i++)
+   {
+        if((string[i] != ' ') && (string[i] < 'a' || string[i] > 'z') && (string[i] < 'A' || string[i] > 'Z'))
+        {
+            valido=0;
+        }
+    }
+   return valido;
+}
+
+int ValidarNumero(char numero[])
+{
+   int valido=1;
+   for(int i=0;i<strlen(numero);i++)
+   {
+	  if(numero[i] < '0' || numero[i] > '9')
+	  {
+		  valido=0;
+	  }
+   }
+   return valido;
+}
+
+void MostrarNominaEmpleadosOrdenadosPorApellidoSector(eEmpleado unEmpleado[],int tamanioarray)
+{
+    eEmpleado auxiliar;
+
+    for(int i=0;i<tamanioarray-1;i++)
+    {
+        for(int j=i+1;j<tamanioarray;j++)
+        {
+            if( strcmp(unEmpleado[i].apellido,unEmpleado[j].apellido)>0 || ( strcmp(unEmpleado[i].apellido,unEmpleado[j].apellido)==0  && unEmpleado[i].sector< unEmpleado[j].sector) )
+            {
+                auxiliar= unEmpleado[i];
+                unEmpleado[j]=unEmpleado[i];
+                unEmpleado[j]=auxiliar;
+            }
+        }
+    }
+
+}
+
+float CalcularTotalSalarios(eEmpleado unEmpleado[],int cantEmpleados)
+{
+  float acumuladorSalarios;
+  acumuladorSalarios=0;
+
+  for(int i=0;i<cantEmpleados;i++)
+  {
+      acumuladorSalarios=acumuladorSalarios+unEmpleado[i].sueldo;
+  }
+  return acumuladorSalarios;
+}
+
+float CalcularPromedioSalarios(eEmpleado unEmpleado[],int cantEmpleados,float salarioTotal)
+{
+    float promedioSalarios;
+    promedioSalarios=salarioTotal/cantEmpleados;
+    return promedioSalarios;
+}
+
+float ContarEmpleadosSueldoMayorAlPromedio(eEmpleado unEmpleado[],int cantEmpleados,float salarioPromedio)
+{
+    int contadorEmpleadosSueldoMayorAlPromedio=0;
+
+    for(int i=0;i<cantEmpleados;i++)
+    {
+        if(unEmpleado[i].sueldo>salarioPromedio)
+        {
+            contadorEmpleadosSueldoMayorAlPromedio++;
+        }
+    }
+
+    return contadorEmpleadosSueldoMayorAlPromedio;
+}
 
 
--- Trabajo Practico numero 2/empleados.h
+++ Trabajo Practico numero 2/empleados.h
@@ -1,5 +1,173 @@
+typedef struct
+{
+ int id;
+ char nombre[51];
+ char apellido[51];
+ float sueldo;
+ int sector;
+ int isEmpty;
+}eEmpleado;
+
+
+/** \brief  Inicializa un array con el valor indicado
+ *
+ * \param [] eEmpleado Es el array a ser inicializado
+ * \param int La longitud de dicho array
+ * \return int El resultado de la inicializacion:
+                                                 - (0)Si fue inicializado exitosamente
+                                                 - (-1)Si no se inicializó
+ *
+ */int initEmployees(eEmpleado [],int);
+
+
+/** \brief Solicita los valores a guardar en una estructura y los guarda en una posición libre
+ *
+ * \param [] eEmpleado El array donde se cargaran los valores
+ * \param int La longitud del array
+ * \param int El indice de la posicion libre donde se cargaran los datos
+ * \return eEmpleado Una copia de todos los valores que se cargaron
+ *
+ */eEmpleado CargarEmpleado(eEmpleado [],int,int);
+
+/** \brief Busca el primer indice libre en un array
+ *
+ * \param [] eEmpleado El array donde se buscará
+ * \param int  La longitud de dicho array
+ * \return int Devuelve -el indice donde puedo guardar el dato, o
+                        - (-1) si no hay espacio
+ *
+ */int BuscarLibre(eEmpleado [],int);
+
+/** \brief Imprime el array de empleados de forma encolumnada.
+ *
+ * \param [] eEmpleado El array a imprimir
+ * \param int  La longitud de dicho array
+ * \return void
+ *
+ */ void MostrarNominaDeEmpleados(eEmpleado [],int );
 
 
 
+void MostrarNominaEmpleadosOrdenadosPorApellidoSector(eEmpleado [],int);
+
+void ModificarUnEmpleado(eEmpleado [],int,int,int);
+
+
+
+
+
+/** \brief Busca un empleado recibiendo como parámetro de búsqueda su Id.
+ *
+ * \param int El id a buscar
+ * \param int El tamaño del array donde lo buscará
+ * \param [] eEmpleado El array donde buscará el id
+ * \return int Devuelve -(-1)Si no encuentra ese id
+                        -El numero del id en caso de encontrarlo
+
+ *
+ */int findEmployeeById(int,int,eEmpleado []);
+
+/** \brief Elimina de manera lógica un empleado recibiendo como parámetro su Id.
+ *
+ * \param [] eEmpleado
+ * \param int El longitud del array(solo la longitud de datos ya cargados)
+ * \param int El id a eliminar
+ * \return int Devuelve -(-1)Si no pudo eliminar
+                        -0 si se elimino correctamente
+
+ *
+ */int removeEmployee(eEmpleado [],int,int);
+
+/** \brief Calcula la suma de todos los sueldos ingresados
+ *
+ * \param [] eEmpleado
+ * \param int Cantidad de empleados cargados
+ * \return float La suma de todos los sueldos ingresados
+ *
+ */ float CalcularTotalSalarios(eEmpleado [],int);
+
+
+/** \brief Calcula el salario promedio de los empleados
+ *
+ * \param [] eEmpleado
+ * \param int Cantidad de empleados cargados
+ * \param float El acumulador de sueldos
+ * \return float Devuelve el sueldo promedio de los empleados
+ *
+ */float CalcularPromedioSalarios(eEmpleado [],int ,float);
+
+/** \brief Cuenta la cantidad de empleados cuyo sueldo es mayor a el sueldo promedio
+ *
+ * \param [] eEmpleado
+ * \param int Cantidad de empleados ingresados
+ * \param float Sueldo promedio
+ * \return float
+ *
+ */float ContarEmpleadosSueldoMayorAlPromedio(eEmpleado [],int ,float);
+
+/** \brief Verifica si el valor recibido contiene solo letras
+ *
+ * \param [] char Array con la cadena a ser analizada
+ * \return int Devuelve - (1) si contiene solo ' ' y letras, o
+                         - (0) en caso de que no
+ *
+ */
+int ValidarLetras(char []);
+
+
+/** \brief Verifica si el valor recibido es numérico
+ *
+ * \param [] char  Array con la cadena a ser analizada
+ * \return int Devuelve - (1) si contiene solo numeros, o
+                        -(0) en caso de que tenga otro tipo de caracteres
+ *
+ */
+int ValidarNumero(char []);
+
+
+/** \brief Verifica si el valor recibido es numérico flotante
+ *
+ * \param numero[] char Array con la cadena a ser analizada
+ * \return int Devuelve  - (1) si contiene solo numeros y ".", o
+                         - (0) en caso de que tenga otro tipo de caracteres
+ *
+ */
+int ValidarFlotante(char numero[]);
+
+
+
+/** \brief Solicita un numero entero y valida que no se ingrese otro tipo de dato
+            (mostrando error y pidiendo reingreso en caso de que no sea asi)
+ *
+ * \param [] char El mensaje a mostrar para solicitar el numero
+ * \param [] char  El mensaje de error que se mostrará en caso de que lo ingresado no sea un numero
+ * \return int Devuelve el numero ingresado
+ *
+ */
+int PedirEntero(char [],char []);
+
+
+/** \brief Solicita un string, valida que contenga caracteres alfabeticos(mostrando error y pidiendo reingreso en caso de que no sea asi)
+ y lo guarda en una variable
+ *
+ * \param [] char El mensaje solicitando el texto
+ * \param [] char El mensaje en caso de error
+ * \param int El tamaño maximo que soporta el array donde se guardara el texto ingresado
+ * \param [] char El array donde se guardará la texto ingresado
+ * \return void
+ *
+ */
+void PedirString(char [],char [],int ,char []);
+
+
+
+/** \brief Solicita un numero flotante, y valida que contenga caracteres numericos(mostrando error y pidiendo reingreso en caso de que no sea asi)
+ *
+ * \param texto[] char El mensaje solicitando el dato
+ * \param textoError[] char El mensaje en caso de error
+ * \return float Devuelve el numero ingresado
+ *
+ */
+float PedirFlotante(char texto[],char textoError[]);
 
 
